2de5fef453bf9e0ffcb6fab09c164f38ee70f416
JaferShar <jafer.shar@gmail.com>
2024-05-03 14:43:52 +0200

    removed sprinting

--- Catch the Fruit/Assets/Code/Player/PlayerMovement.cs
+++ Catch the Fruit/Assets/Code/Player/PlayerMovement.cs
@@ -6,9 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
         public Rigidbody2D rb;
-        public float baseSpeed = 4.5f;
-        public float sprintSpeed = 7f;
-
+        private float baseSpeed = 9f;
         private Vector2 move;
         private float currMoveSpeed;
         
@@ -25,14 +23,5 @@ public class PlayerMovement : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         move = context.ReadValue<Vector2>();
-    }
-
-    public void Sprint(InputAction.CallbackContext context)
-    {
-        if (context.started) {
-            currMoveSpeed = sprintSpeed;
-        } else if (context.canceled) {
-            currMoveSpeed = baseSpeed;
-        }
     }
 }
